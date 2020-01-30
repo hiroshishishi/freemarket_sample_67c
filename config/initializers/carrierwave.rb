279ede11b89ec3395162e334634935d9549d1c2d
takumi-915 <59188083+takumi-915@users.noreply.github.com>
2020-01-30 20:42:00 +0900

    carrierwave・Basic認証の導入

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,20 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production? #本番はS3に保存する
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',     #AWSのアクセスキーとシークレットキーを環境変数で定義する
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id], #credentails.ymlに鍵の本体があります
+      aws_secret_access_key: Rails.application.credentials.exitaws[:secret_access_key],  #credentails.ymlに鍵の本体があります
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'freemarket67c'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket67c'
+  else
+    config.storage = :file
+  end
+end
