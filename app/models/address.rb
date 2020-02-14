02bf1a5db98baf20be2ca02f722ecd2d7f3acc46
hiroshishishi <364453hiroshi@gmail.com>
2020-02-14 22:01:38 +0900

    バリデーション新規

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,13 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to :user
-  # validates :lastname, :firstname, :lastname_kana, :firstname_kana, :zip_code, :prefecture_id, :city, :street, presence: true
-  # validates :telephone, uniqueness: true
+  validates :lastname, presence: true
+  validates :telephone, uniqueness: true
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :prefecture_id, presence: true
+  validates :zip_code, presence: true
+  validates :firstname_kana, presence: true
+  validates :lastname_kana, presence: true
+  validates :firstname, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,15 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :social_providers, dependent: :destroy
   has_one :address, dependent: :destroy
-  # validate :nickname, :lastname, :firstname, :lastname_kana, :firstname_kana, :birth_year, :birth_month, :birth_day, :password, presence: true
-  # validate :email, uniqueness: true, presence: true
-  # validate :telephone, uniqueness: true
+  validate :email, uniqueness: true, presence: true
+  validate :telephone, uniqueness: true
+  validate :password, presence: true
+  validate :nickname, presence: true
+  validate :lastname, presence: true
+  validate :firstname, presence: true
+  validate :lastname_kana, presence: true
+  validate :firstname_kana, presence: true
+  validate :birth_year, presence: true
+  validate :birth_month, presence: true
+  validate :birth_day, presence: true
 end
