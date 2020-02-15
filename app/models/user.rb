619e6dbc3315451976c820ded48d995852d12eda
hiroshishishi <364453hiroshi@gmail.com>
2020-02-15 23:23:03 +0900

    新規バリデーション修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,15 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :social_providers, dependent: :destroy
   has_one :address, dependent: :destroy
-  validate :email, uniqueness: true, presence: true
-  validate :telephone, uniqueness: true
-  validate :password, presence: true
-  validate :nickname, presence: true
-  validate :lastname, presence: true
-  validate :firstname, presence: true
-  validate :lastname_kana, presence: true
-  validate :firstname_kana, presence: true
-  validate :birth_year, presence: true
-  validate :birth_month, presence: true
-  validate :birth_day, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :telephone, uniqueness: true
+  validates :password, presence: true
+  validates :nickname, presence: true
+  validates :lastname, presence: true
+  validates :firstname, presence: true
+  validates :lastname_kana, presence: true
+  validates :firstname_kana, presence: true
+  validates :birth_year, presence: true
+  validates :birth_month, presence: true
+  validates :birth_day, presence: true
 end
