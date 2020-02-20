1d19d432cc1de915a0689286d3dbbe31fa8c5fd9
kuromame0104 <newgene4865@icloud.com>
2020-02-20 19:55:20 +0900

    turbolinks修正

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,11 +1,9 @@
-$(document).on('turbolinks:load', ()=> {
-  $(function() {
-    $('img.image01').click(function(){
-      var ImgSrc = $(this).attr("src");
-      $("#mainphoto").attr("src",ImgSrc);
-      $("#mainphoto").hide();
-      $("#mainphoto").fadeIn();
-        return false;
-    });
+$(function() {
+  $('img.image01').click(function(){
+    var ImgSrc = $(this).attr("src");
+    $("#mainphoto").attr("src",ImgSrc);
+    $("#mainphoto").hide();
+    $("#mainphoto").fadeIn();
+      return false;
   });
 });
