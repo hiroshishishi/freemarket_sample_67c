ea91a04a106e434657073b242b35358596e678dd
kuromame0104 <newgene4865@icloud.com>
2020-02-20 19:28:44 +0900

    商品編集機能修正

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,5 +1,5 @@
-$(document).on('turbolinks:load', function(){
-  $(function(){
+if(document.URL.match(/new/)){
+  $(document).on('turbolinks:load', function(){
     function buildHTML(count) {
       var html = `<div class="preview-box" id="preview-box__${count}">
                   <div class="upper-box">
@@ -94,4 +94,4 @@ $(document).on('turbolinks:load', function(){
       }
     });
   });
-});
+};
