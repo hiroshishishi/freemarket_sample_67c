46682676fc493451db90822d0a7cb54a8f9db340
matsushimayuki5225 <58546697+matsushimayuki5225@users.noreply.github.com>
2020-02-20 15:02:40 +0900

    商品削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   root to: 'items#index'
-  resources :items, only: [:index, :show, :new, :create] do
+  resources :items, only: [:index, :show, :new, :create,:destroy] do
     #Ajaxで動くアクションのルートを作成
     collection do
       get 'get_category_children', defaults: { format: 'json' }
